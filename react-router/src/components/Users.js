2a4c77f91bdfe3a5d29e3c8255739e13ed412465
Saurabh Chandil <saurabh.chandil2013@gmail.com>
2022-03-18 20:46:02 +0530

    search params

--- react-router/src/components/Users.js
+++ react-router/src/components/Users.js
@@ -1,15 +1,27 @@
 import React from 'react'
-import { NavLink, Outlet } from 'react-router-dom'
+import { NavLink, Outlet, useSearchParams } from 'react-router-dom'
 
 function Users() {
-  return (
-    <div>
-        <NavLink to='1'><h2>User 1</h2></NavLink>
-        <NavLink to='2'><h2>User 2</h2></NavLink>
-        <NavLink to='3'><h2>User 3</h2></NavLink>
-        <Outlet />
-    </div>
-  )
+    const [ searchParams, setSearchParams ] = useSearchParams()
+    const showActiveUsers = searchParams.get('filter') === 'active' 
+    return (
+        <div>
+            <NavLink to='1'><h2>User 1</h2></NavLink>
+            <NavLink to='2'><h2>User 2</h2></NavLink>
+            <NavLink to='3'><h2>User 3</h2></NavLink>
+            <Outlet />
+
+            <div>
+                <button
+                onClick={() => setSearchParams({ filter: 'active' })}>Active Users</button>
+                <button 
+                onClick={() => setSearchParams({ })}>Reset Users</button>
+            </div>
+            {
+                showActiveUsers ? <h2>Showing active users</h2> : <h2>Showing all users</h2>
+            }
+        </div>
+    )
 }
 
 export default Users
